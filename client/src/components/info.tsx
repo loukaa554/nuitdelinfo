94075f9df76a1cf333440649b295b80ea57b1876
Loukaa <louka.mourrier@gmail.com>
2024-12-05 21:53:24 +0100

    v0

---
+++ client/src/components/info.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function info() {
+  return <div>info</div>;
+}
+
+export default info;
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -4,9 +4,16 @@ import Human from "../../assets/human.png";
 function Home() {
   return (
     <div className=" flex">
-      <div className="w-1/2">
-        <img className="w-1/2 h-10" src={Human} alt="" />
-      </div>
+      <div
+        className="w-1/2"
+        style={{
+          width: "50vw",
+          height: "150vh",
+          backgroundImage: `url(${Human})`,
+          backgroundSize: "cover",
+          backgroundPosition: "right",
+        }}
+      ></div>
       <div>
         <div>
           <Title4>Salut</Title4>
