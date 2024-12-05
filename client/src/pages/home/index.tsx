2ae2bd3936617ea31d4cd4fcf09d3ba4afcc56f3
Loukaa <louka.mourrier@gmail.com>
2024-12-05 21:38:54 +0100

    init

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { P, Title4 } from "@/components/typographie";
+import Human from "../../assets/human.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className=" flex">
+      <div className="w-1/2">
+        <img className="w-1/2 h-10" src={Human} alt="" />
+      </div>
+      <div>
+        <div>
+          <Title4>Salut</Title4>
+          <P>Je suis un</P>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
